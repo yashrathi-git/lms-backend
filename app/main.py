62f7483f1f5031ac88d153567f5701b892ddc8a9
Yash Rathi <yashrathicricket@gmail.com>
2024-03-14 00:10:04 +0530

    Upadate open API key

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import os
 from fastapi import FastAPI, HTTPException
 from openai import OpenAI
-
+from dotenv import load_dotenv
 from app.models import CurriculumRequest
+import os
 
+load_dotenv()
 
-OPENAI_API_KEY = "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"
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
