09d97d5c1920090b92237bcaf410f7638afed331
Yash Rathi <yashrathicricket@gmail.com>
2024-03-17 04:38:34 +0530

    some changes

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 
 origins = [
-    "http://127.0.0.1:5500",
+    "*",
 ]
 
 
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app.main import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True)
+    uvicorn.run("app.main:app", reload=True, host="0.0.0.0")
